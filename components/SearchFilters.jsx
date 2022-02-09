21552afc1988ba226dc296f49f50741a86a01bab
Sheelove Dol <siesel26@gmail.com>
2022-02-08 17:08:43 -0800

    Add loading bar

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -13,10 +13,13 @@ const SearchFilters = () => {
     const path = router.pathname;
     const { query } = router;
 
+
     const values = getFilterValues(filterValues);
     
     values.forEach((item) => {
-      query[item.name] = item.value
+      if (item.value && filterValues?.[item.name]) {
+        query[item.name] = item.value
+      }
     })
 
     router.push({ pathname: path, query })
